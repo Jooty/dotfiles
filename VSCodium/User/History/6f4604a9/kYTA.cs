0a88a1e34b8087728a58f7cba79a4e94deb67ba0
Jay <compundbread@gmail.com>
2023-04-09 16:45:41 -0400

    init commit

---
+++ VSCodium/User/History/6f4604a9/kYTA.cs
@@ -0,0 +1,15 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Imlight.Common;
+
+namespace Imlight.Net
+{
+    public class Server : ReceiveActor
+    {
+        
+    }
+}
